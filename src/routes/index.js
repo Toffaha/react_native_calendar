1e451748280d0f1dd1bb46ca0149f04c8f57e71f
Toffaha <omar.mayani@gmail.com>
2019-01-01 21:50:42 +0200

    happy with progress :))

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,26 @@
-import {createStackNavigator} from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation'
+import Icon from 'react-native-ionicons'
 import Perus from './perus'
+import React from 'react'
 
-export const AppNavigator = createStackNavigator({
-    Home: Perus,
+export const AppNavigator = createBottomTabNavigator({
+    Calendar: Perus,
+    
+}, {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        let iconName
+        switch (navigation.state.routeName) {
+            case 'Calendar':
+                iconName='grid'
+            default:
+                break;
+        }
+        return <Icon name={iconName} />
+      },  
+    }),
+    tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'green'
+    }
 })
